a6864c3486723518a73b28afa2e6d50bd4c37e5e
alex <adolara@sigma-rail.com>
2021-04-30 15:02:03 +0200

    rollback

--- js/random.js
+++ js/random.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    var classCycle=['img1','img2','img3','img4','img5','img6','img7'];
+    var classCycle=['img1','img2','img3','img4','img5'];
 
     var randomNumber = Math.floor(Math.random() * classCycle.length);
-    var classToAdd = img[randomNumber];
+    var classToAdd = classCycle[randomNumber];
     
     $('body').addClass(classToAdd);
 
